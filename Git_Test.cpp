c6e3c9e8da992c60aaddf8c9ea35514d80b070e7
光輝 山本 <k025g0119@g.neec.ac.jp>
2025-10-18 13:19:09 +0900

    新規ブランチで文章を追加してコミット

--- Git_Test.cpp
+++ Git_Test.cpp
@@ -5,7 +5,9 @@
 
 int main()
 {
-    std::cout << "Hello Visual Stdio!\n";
+    std::cout << "Hello GitHub World!\n";
+
+    std::cout << "Hello New branch!\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
